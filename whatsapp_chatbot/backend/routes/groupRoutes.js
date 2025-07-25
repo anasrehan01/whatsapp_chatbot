d70a7c8a9f1c05c4189c8b4c2a828cd6845bd796
anasrehan01 <codewithanas01@gmail.com>
2025-07-25 16:54:47 +0500

    Delete whatsapp_chatbot/backend directory

--- whatsapp_chatbot/backend/routes/groupRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const groupController = require('../controller/groupController');
-
-router.get('/', groupController.getGroups);
-
-router.post('/sync', groupController.syncGroups);
-
-module.exports = router;
